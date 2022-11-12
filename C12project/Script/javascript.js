f1983d1f91b4cc5447c51c5506623df5c692e925
SobamboS <sobamboolusola1152@gmail.com>
2022-11-12 15:23:28 +0100

    first commit

---
+++ C12project/Script/javascript.js
@@ -0,0 +1,67 @@
+// console.log('Hello World')
+// console.log("Sobambo Olusola")
+// name='Sobambo Olusola'
+// console.log(name)
+
+
+function calculateTipAmount(food,tip){
+ tipPercentage = tip / 100
+ tipAmount = food * tipPercentage
+ total = sum(food,tip)
+ return total
+}
+console.log(calculateTipAmount(100, 20))
+console.log(total)
+
+
+// car = "Volvo"
+// console.log(car)
+// food= Number(prompt('How much is the food'))
+// alert(food)
+
+// const cars = ["Saab", "Volvo", "BMW"]
+// cars[0]
+
+
+// let name = prompt("enter your name ?");
+// console.log('Hi my name is ${name} am a grown man');
+
+
+//  let food = "rice ";
+// let car = "volvo ";
+// let ball = "jabulani";
+
+// console.log(food + car + ball)
+ 
+
+// p1 = 6
+// p2 = 3
+// p3 = 3
+
+// function level(p1, p2, p3){
+// return (p1 + p2 + p3)}
+
+// console.log(level(p1, p2,p3))
+
+// Weather App
+// let weather = "rain"
+// if(weather == "rain"){
+//     console.log("Grab your umbrella")
+// }else{
+//     console.log("Wear your sunglasses")
+// }
+
+// function
+
+// function sayMyName(name){
+//   greet = `My name is ${name}.`
+//   console.log(greet)
+// }
+// sayMyName("Sobambo Olusola")
+
+
+function sum(a, b){
+  return a * b
+}
+// num=sum(7,2)
+// console.log(num)
